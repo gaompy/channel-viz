9cd3c38b5bce3c1ef352578c39a9983ae5787952
Guillermo Osorio <gaompy@gmail.com>
2015-10-16 18:45:08 -0300

    Actualizando la llave de Xively

--- js/follows.js
+++ js/follows.js
@@ -12,7 +12,7 @@
      hideForm		= 0;
      */
 
-    var defaultKey = 'QKqFMDVPCnskjPSQrIrjeDJ0I8P0YRFsh0HA0BRixkNVrcqL', // Unique master Xively API key to be used as a default
+    var defaultKey = 'UYXhKeIzdrQtKf158TmKKzT7Eo8uOuAt7abJBmessNJgqJUA', // Unique master Xively API key to be used as a default
             defaultFeeds = [1439966730], // Comma separated array of Xively Feed ID numbers
             applicationName = '', // Replaces Xively logo in the header
             dataDuration = '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
